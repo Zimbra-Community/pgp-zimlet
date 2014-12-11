927921e4001c9f99ad4d94fd619ea7a20a90626a
Barry de Graaff <info@barrydegraaff.tk>
2014-12-11 09:27:46 +0100

    adding fingerprint and create date in encrypt window, when user points recipients.

--- tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
+++ tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
@@ -780,7 +780,7 @@ function() {
 				for (i = 0; i < entry[0].users.length; i++) {
 					userid = entry[0].users[i].userId.userid.replace(/\</g,"&lt;");
 					userid = userid.replace(/\>/g,"&gt;") ;
-					result = result + '<option value="'+entry[0].armor()+'">'+userid+'</option>';
+					result = result + '<option title="fingerprint: '+entry[0].primaryKey.fingerprint+' \r\ncreated: '+entry[0].primaryKey.created+'" value="'+entry[0].armor()+'">'+userid+'</option>';
 				}
          }
       });
