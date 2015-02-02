b60e333540f4022839c67eb633319921a2668878
Barry de Graaff <info@barrydegraaff.tk>
2015-02-02 08:44:37 +0100

    removing some left-over html/css from decrypted/verified result, only show scrollbars if needed.

--- conf.mk
+++ conf.mk
@@ -1,4 +1,4 @@
-VERSION=1.4.1
+VERSION=1.4.2
 PACKAGE=zcs-openpgp
 MAINTAINER=Barry de Graaff <info@barrydegraaff.tk>
 SECTION=free/zcs-mail
--- tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
+++ tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
@@ -294,7 +294,7 @@ tk_barrydegraaff_zimbra_openpgp.prototype.verify = function(message) {
          }
          if (message.text.indexOf('<html><body>') > -1 ) 
          {       
-            myWindow.displayDialog(2, 'Signed message ' + sigStatus, '<div style="width:650px; height: 350px; overflow-x: hidden; overflow-y: scroll; background-color:white; padding:5px;"><div contenteditable="true" class="barrydegraaff_zimbra_openpgp-msg" style="height:320px;">'+message.text+'</div></div>');
+            myWindow.displayDialog(2, 'Signed message ' + sigStatus, '<div style="width:650px; height: 350px; overflow-x: auto; overflow-y: auto; background-color:white; padding:5px;" contenteditable="true">'+message.text+'</div>');
          }
       },
       function (err) {
@@ -591,7 +591,7 @@ function() {
                   var preClose = '';
                }
                myWindow._dialog.setTitle('Decrypted message '+ sigStatus);
-               myWindow._dialog.setContent('<div style="width:650px; height: 350px; overflow-x: scroll; overflow-y: scroll; background-color:white; padding:5px;"><div contenteditable="true" class="barrydegraaff_zimbra_openpgp-msg" style="height:320px;">'+ preOpen + decrypted.text + preClose +'</div></div><br><small><a id="original-a" onclick="document.getElementById(\'openpgp-original\').style.display = \'inline\';document.getElementById(\'original-a\').style.display = \'none\';">original</a></small><textarea id="openpgp-original" class="barrydegraaff_zimbra_openpgp-msg" style="height:40px; display:none">'+decrypted.text+'</textarea>');
+               myWindow._dialog.setContent('<div style="width:650px; height: 350px; overflow-x: auto; overflow-y: auto; background-color:white; padding:5px;" contenteditable="true">'+ preOpen + decrypted.text + preClose +'</div><br><small><a id="original-a" onclick="document.getElementById(\'openpgp-original\').style.display = \'inline\';document.getElementById(\'original-a\').style.display = \'none\';">original</a></small><textarea id="openpgp-original" class="barrydegraaff_zimbra_openpgp-msg" style="height:40px; display:none">'+decrypted.text+'</textarea>');
             },
             function(err) {
                tk_barrydegraaff_zimbra_openpgp.prototype.status("Decryption failed!", ZmStatusView.LEVEL_WARNING);
