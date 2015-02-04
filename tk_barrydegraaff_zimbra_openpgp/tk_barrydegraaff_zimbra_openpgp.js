ee34f2c6c4cd30c61720949824e6e82e5a7d7ba0
Barry de Graaff <info@barrydegraaff.tk>
2015-02-04 09:51:49 +0100

    adding functions for decoding base64 attachment and save them using browser. downloadBlob, b64ToUint6, base64DecToArr

--- tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
+++ tk_barrydegraaff_zimbra_openpgp/tk_barrydegraaff_zimbra_openpgp.js
@@ -1215,3 +1215,64 @@ tk_barrydegraaff_zimbra_openpgp.prototype._waitForContactToLoadAndProcess = func
    catch(err) {
    }
 }
+
+/* Attachment integration
+ * Decode and download a base64 encoded attachment
+ * */
+tk_barrydegraaff_zimbra_openpgp.prototype.downloadBlob = function (filename, type, base64Data) {
+   filename = filename ? filename : 'file.bin';
+   type = type ? type : 'octet/stream';
+   var dataBin = tk_barrydegraaff_zimbra_openpgp.prototype.base64DecToArr(base64Data);
+   var blob = new Blob([dataBin], { type: type });
+   
+   if (!window.navigator.msSaveOrOpenBlob) 
+   {
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = filename;
+      a.click();
+   }
+   else
+   {
+      window.navigator.msSaveOrOpenBlob(blob, filename);
+   }
+}
+
+/* Base64 decode binary safe
+ https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+*/
+tk_barrydegraaff_zimbra_openpgp.prototype.b64ToUint6 = function (nChr) {
+  return nChr > 64 && nChr < 91 ?
+      nChr - 65
+    : nChr > 96 && nChr < 123 ?
+      nChr - 71
+    : nChr > 47 && nChr < 58 ?
+      nChr + 4
+    : nChr === 43 ?
+      62
+    : nChr === 47 ?
+      63
+    :
+      0;
+}
+
+tk_barrydegraaff_zimbra_openpgp.prototype.base64DecToArr = function (sBase64, nBlocksSize) {
+  var
+    sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+    nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2, taBytes = new Uint8Array(nOutLen);
+
+  for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+    nMod4 = nInIdx & 3;
+    nUint24 |= tk_barrydegraaff_zimbra_openpgp.prototype.b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+    if (nMod4 === 3 || nInLen - nInIdx === 1) {
+      for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+        taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+      }
+      nUint24 = 0;
+    }
+  }
+  return taBytes;
+}
