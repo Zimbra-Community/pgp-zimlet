ad2ed019a8ae8c9922c16df715890528ae113ee6
barrydegraaff <info@barrydegraaff.tk>
2015-04-01 07:14:28 +0200

    fixing wrong commit of virt-install

--- kvm/virt-install.sh
+++ kvm/virt-install.sh
@@ -10,15 +10,14 @@ virt-install \
   --connect qemu:///system \
   --hvm \
   --virt-type kvm \
-  --network=default,model=virtio,mac=52:54:00:93:28:27 \
+  --network=default,model=virtio,mac=52:54:00:d4:22:bf \
   --noautoconsole \
-  --name windows7 \
-  --disk path=/dev/vg_dev/windows7-disk1,bus=ide,cache=none \
+  --name zimbra-dev \
+  --disk path=/dev/vg_dev/zimbra-dev-disk1,bus=virtio,cache=none \
   --ram 2048 \
   --vcpus=4\
   --vnc \
-  --os-type windows \
-  --os-variant win7 \
-  --cdrom /dev/sr0
+  --os-type linux \
+  --os-variant rhel6 \
   --location http://ftp.tudelft.nl/centos.org/7/os/x86_64/ \
   -x "ks=https://raw.githubusercontent.com/barrydegraaff/pgp-zimlet/master/kvm/centos7.cfg"
