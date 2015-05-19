4f9410114c664a64608cf1807df54e45338ad1c6
Barry de Graaff <info@barrydegraaff.tk>
2015-05-19 15:53:12 +0200

    Release version 1.6.8, drop support for zmpkg, as I have no use for this.

--- conf.mk
+++
@@ -1,7 +0,0 @@
-VERSION=1.6.7
-PACKAGE=zcs-openpgp
-MAINTAINER=Barry de Graaff <info@barrydegraaff.tk>
-SECTION=free/zcs-mail
-PRIORITY=optional
-DEPENDS=
-DESCRIPTION=Encrypt/Decrypt messages with OpenPGP
