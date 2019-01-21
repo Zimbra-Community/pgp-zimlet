3d50e2cbe8f842abc99da7e11cf38320257f5cdc
Barry de Graaff <info@barrydegraaff.tk>
2019-01-21 19:08:15 +0100

    Update virt-install.sh

--- kvm/virt-install.sh
+++ kvm/virt-install.sh
@@ -21,3 +21,26 @@ virt-install \
   --os-variant rhel6 \
   --location http://ftp.tudelft.nl/centos.org/7/os/x86_64/ \
   -x "ks=https://raw.githubusercontent.com/Zimbra-Community/pgp-zimlet/stable/kvm/centos7-ext4.cfg"
+
+
+
+
+# Same thing, but without LVM on the host:
+
+qemu-img create -f qcow2 /media/zimbra-dev.qcow2 20G
+
+virt-install \
+  --connect qemu:///system \
+  --hvm \
+  --virt-type kvm \
+  --network=default,model=virtio,mac=52:54:00:d4:22:bf \
+  --noautoconsole \
+  --name zimbra-dev \
+  --disk path=/media/zimbra-dev.qcow2,bus=virtio,cache=none \
+  --ram 6000 \
+  --vcpus=4\
+  --vnc \
+  --os-type linux \
+  --os-variant rhel6 \
+  --location http://ftp.tudelft.nl/centos.org/7/os/x86_64/ \
+  -x "ks=https://raw.githubusercontent.com/Zimbra-Community/pgp-zimlet/stable/kvm/centos7-ext4.cfg"
